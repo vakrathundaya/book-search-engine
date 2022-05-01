ba9f46bfc230f754cea179d93fb2d8cf4f0db028
Radhika <cu9rad@gmail.com>
2022-04-30 20:16:18 -0400

    checking

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -12,13 +12,12 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser, { error }] = useMutation(ADD_USER); 
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
-
-  const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
